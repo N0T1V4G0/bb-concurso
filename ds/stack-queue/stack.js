b329ec8c3f2fa0e03ddd59915f8dc54ff2aca27b
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-03-03 04:17:24 -0300

    add peek & push mtds to Stack class

--- ds/stack-queue/stack.js
+++ ds/stack-queue/stack.js
@@ -11,4 +11,32 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+      this.length++;
+      return this;
+    }
+
+    newNode.next = this.top;
+    this.top = newNode;
+    this.length++;
+    return this;
+  }
 }
+
+const st = new Stack();
+st.push('hey');
+st.push('hey2');
+st.push('hey3');
+st.push('hey4');
+st.peek();
+console.log(st);
